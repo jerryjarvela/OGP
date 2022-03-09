ef41b10d6b76af1d4ef9d28444c1246e632ea133
Jerryjarvela <AA2890@student.jamk.fi>
2022-03-09 10:07:37 +0200

    Forgot to add git when I started

---
+++ SimpleChatClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.Connect(new IPEndPoint(IPAddress.Parse("91.156.58.156"), 88));
+
+        // Reading the server message
+        byte[] data = new byte[1024];
+        int count = clientSocket.Receive(data);
+        string msgReceive = Encoding.UTF8.GetString(data, 0, count);
+        Console.WriteLine(msgReceive);
+
+        while(true)
+        {
+            // Sending message to server
+            string msgSend = Console.ReadLine();
+            clientSocket.Send(Encoding.UTF8.GetBytes(msgSend));
+        }
+
+        Console.ReadKey();
+        clientSocket.Close();
+    }
+}
---
+++ SimpleChatServer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static Socket serverSocket, clientSocket;
+    static IPEndPoint ipEndpoint;
+    static IPAddress ipAddress;
+    static int port = 88;
+    static byte[] dataBuffer = new byte[1024];
+
+    static void Main(string[] args)
+    {
+        ASyncServer();
+    }
+
+    private static void SyncServer()
+    {
+        // Configs
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ipAddress = IPAddress.Parse("91.156.58.156");
+        ipEndpoint = new IPEndPoint(ipAddress, port);
+        serverSocket.Bind(ipEndpoint);
+        serverSocket.Listen(0);
+        clientSocket = serverSocket.Accept();
+
+        // Sending message to client
+        string msg = "Hello client, from server";
+        byte[] Byte = Encoding.UTF8.GetBytes(msg);
+        clientSocket.Send(Byte);
+
+        // Read message from client
+        byte[] datBuffer = new byte[1024];
+        int count = clientSocket.Receive(datBuffer);
+        string msgReveice = Encoding.UTF8.GetString(datBuffer, 0, count);
+        Console.WriteLine(msgReveice);
+
+        // Close sockets
+        Console.ReadKey();
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+
+    private static void ASyncServer()
+    {
+        // Configs
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ipAddress = IPAddress.Parse("91.156.58.156");
+        ipEndpoint = new IPEndPoint(ipAddress, port);
+        serverSocket.Bind(ipEndpoint);
+        serverSocket.Listen(0);
+        clientSocket = serverSocket.Accept();
+
+        // Sending message to client
+        string msg = "Hello client, from server";
+        byte[] Byte = Encoding.UTF8.GetBytes(msg);
+        clientSocket.Send(Byte);
+
+        // Read message from client
+        clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallback, null);
+        
+
+        // Close sockets
+        Console.ReadKey();
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+
+    private static void ReceiveCallback(IAsyncResult ar)
+    {
+        int count = clientSocket.EndReceive(ar);
+        string msgReceive = Encoding.UTF8.GetString(dataBuffer, 0, (int)count);
+        Console.WriteLine(msgReceive);
+        clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallback, null);
+    }
+}
